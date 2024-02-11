b3cee23553dcbf20c305729e089ee0deaf7325b8
zahidsammy <zahidsammy95@gmail.com>
2024-02-11 21:41:39 +1300

    Tesla App Clone

---
+++ components/CarItem/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {View, Text, ImageBackground} from 'react-native';
+import styles from './style';
+import StyledButton from "../StyledButton";
+
+
+const CarItem = (props) => {
+    return (
+        <View style={styles.carContainer}>
+
+        <ImageBackground 
+        source = {require('../../assets/images/ModelS.jpeg')}
+        style={styles.image}
+        />
+
+
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}> Starting at $69,240</Text>
+        </View>
+
+        <StyledButton type="primary" content={"Custom Order"} onPress={()=> {console.warn (data, "Customer order was pressed")}} />
+
+        <StyledButton type="secondary" content={"Existing Inventory"} onPress={()=> {console.warn (data, "Existing inventory was pressed")}} />
+
+
+        
+      </View>
+    )
+};
+
+export default CarItem;
